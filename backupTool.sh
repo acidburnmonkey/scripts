a25941a836fa468b0a54a5ecf8815cc0447bd888
acidburn <acidburnmonkey@gmail.com>
2023-02-06 02:38:36 -0500

    xp0

--- backupTool.sh
+++ backupTool.sh
@@ -3,32 +3,55 @@
 ## add more folders here -u== no overwrite duplicates ##
 
      # mouts the hhd
-sudo mount /dev/sdb1 /media/2tb
+sudo mount /dev/sdc1 /media/2tb
 sleep 2
-    #the folders to backup 
-cp -r -u /media/Toxic/calendar2022_generated.jpg /media/2tb/backup
-cp -r -u /media/Toxic/camaro\ payments /media/2tb/backup
-cp -r -u /media/Toxic/deck /media/2tb/backup
-cp -r -u /media/Toxic/el\ tatoo /media/2tb/backup
-cp -r -u /media/Toxic/Firmware\ 14.0.0.zip /media/2tb/backup
-cp -r -u /media/Toxic/fitness /media/2tb/backup
-cp -r -u /media/Toxic/Games /media/2tb/backup
-cp -r -u /media/Toxic/important\ documents /media/2tb/backup
-cp -r -u /media/Toxic/linuxbackup /media/2tb/backup
-cp -r -u /media/Toxic/manuals /media/2tb/backup
-cp -r -u /media/Toxic/Monero\ Wallet /media/2tb/backup
-cp -r -u /media/Toxic/Notes /media/2tb/backup
-cp -r -u /media/Toxic/orders /media/2tb/backup
-cp -r -u /media/Toxic/programs /media/2tb/backup
-cp -r -u /media/Toxic/video\ projects /media/2tb/backup
-cp -r -u /media/Toxic/photos /media/2tb/backup
+
+case $1 in
+    #old script
+    -o) 
+
+  #the folders to backup 
+cp -r -u /media/toxic/calendar2022_generated.jpg /media/2tb/backup
+cp -r -u /media/toxic/camaro\ payments /media/2tb/backup
+cp -r -u /media/toxic/el\ tatoo /media/2tb/backup
+cp -r -u /media/toxic/Firmware\ 14.0.0.zip /media/2tb/backup
+cp -r -u /media/toxic/fitness /media/2tb/backup
+cp -r -u /media/toxic/Games /media/2tb/backup
+cp -r -u /media/toxic/important\ documents /media/2tb/backup
+cp -r -u /media/toxic/linuxbackup /media/2tb/backup
+cp -r -u /media/toxic/manuals /media/2tb/backup
+cp -r -u /media/toxic/Monero\ Wallet /media/2tb/backup
+cp -r -u /media/toxic/Notes /media/2tb/backup
+cp -r -u /media/toxic/orders /media/2tb/backup
+cp -r -u /media/toxic/programs /media/2tb/backup
+cp -r -u /media/toxic/video\ projects /media/2tb/backup
+cp -r -u /media/toxic/photos /media/2tb/backup
 
 # Home backups
 cp -r -u ~/scripts /media/2tb/backup
 cp -r -u linux-comands.txt /media/2tb/backup
+cp -r -u ~/PycharmProjects /media/2tb/backup
 
- #-moved
-  
-    # -l will unmount the hhd when not bussy 
+;;
+    
+    -s)
+        sudo rsync --progress -aAX --delete --exclude={/media/toxic/ISO/*} /media/toxic /media/2tb/backup
+        sudo rsync --progress -aAX --delete ~/Nextcloud /media/2tb/backup 
+        sudo rsync --progress -aAX --delete ~/PycharmProjects /media/2tb/backup 
+        sudo rsync --progress -aAX --delete ~/scripts /media/2tb/backup 
+        sudo rsync --progress -aAX --delete ~/stuff /media/2tb/backup 
+        sudo rsync --progress -aAX --delete ~/programs /media/2tb/backup 
+        sudo rsync --progress -aAX --delete ~/linux-comands.txt /media/2tb/backup 
+
+
+        ;;
+        
+    *)
+        echo "No option selected -s or -o"
+        ;;  
+ esac
+
+
+ # -l will unmount the hhd when not bussy 
 sudo umount -l /media/2tb
 echo "All Done"
