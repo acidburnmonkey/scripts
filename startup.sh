3eab003170bc5ebb65b946564bbd4b00170e1acb
acidburn <acidburnmonkey@gmail.com>
2023-03-13 00:09:01 -0400

    plk

--- startup.sh
+++ startup.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
 sleep 2 
 pkill openrgb
+
+#poltit
+exec nohup /usr/libexec/polkit-gnome-authentication-agent-1 &
+
+#remove photoshop garbage files
+rm ~/Photoshop_*.log 
+rm ~/sniffer_*.log
+rm ~/CEPHtmlEngine*
+###############
+
