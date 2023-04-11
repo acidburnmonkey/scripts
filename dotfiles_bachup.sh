2294ce78392c8da8eaef90fd16bd53449a6cc4d3
acidburn <acidburnmonkey@gmail.com>
2023-04-10 23:45:00 -0400

    x00

--- dotfiles_bachup.sh
+++ dotfiles_bachup.sh
@@ -7,8 +7,6 @@ cp -r -u -v ~/.zshrc ~/repos/dotfiles
 cp -r -u -v ~/.config/kitty ~/repos/dotfiles
 #ulauncher
 cp -r -u -v ~/.config/ulauncher ~/repos/dotfiles
-#libreoffice
-cp -r -u -v ~/.config/libreoffice ~/repos/dotfiles
 #neofetch
 cp -r -u -v ~/.config/neofetch ~/repos/dotfiles
 # Cinnamon configs
