d1d55d3dda3d824ef1d5634620fee86e0e10f87f
acidburn <acidburnmonkey@gmail.com>
2023-10-16 22:45:25 -0400

    zz

--- startup.sh
+++ startup.sh
@@ -6,6 +6,9 @@ pkill openrgb
 #poltit
 exec nohup /usr/libexec/polkit-gnome-authentication-agent-1 &
 
+#map caps to esc
+setxkbmap -option "caps:escape"
+
 #remove photoshop garbage files
 rm ~/Photoshop_*.log 
 rm ~/sniffer_*.log
