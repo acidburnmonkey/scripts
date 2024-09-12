82a69e64d4909a923c6a7876841a2634a802c7ee
mal0 <acidburnmonkey@gmail.com>
2024-09-12 08:43:27 -0400

    created case for laptop

--- dotfiles_bachup.sh
+++ dotfiles_bachup.sh
@@ -3,45 +3,53 @@
 dotfiles="$HOME/repos/dotfiles/"
 
 list=(
-#zhrc
-" $HOME/.zshrc"
-#.p10k.zh
-" $HOME/.p10k.zsh"
-#kitty
-"$HOME/.config/kitty "
-#ulauncher
-"$HOME/.config/ulauncher"
-#neofetch
-"$HOME/.config/neofetch"
-# Nvim
-"$HOME/.config/nvim "
-# mpv
-"$HOME/.config/mpv/"
-#vimrc
-"$HOME/.vimrc"
-#hyprland
-"$HOME/.config/hypr/"
-#systemd user
-"$HOME/.config/systemd/"
-#waybar
-"$HOME/.config/waybar/"
-# dunst
-"$HOME/.config/dunst/"
-# Ranger
-"$HOME/.config/ranger/"
-# Rofi
-"$HOME/.config/rofi/"
-# GTK
-"$HOME/.config/gtk*"
+    "$HOME/.zshrc"            # zsh config
+    "$HOME/.p10k.zsh"         # powerlevel10k config
+    "$HOME/.config/kitty"     # kitty terminal config
+    "$HOME/.config/ulauncher" # ulauncher config
+    "$HOME/.config/neofetch"  # neofetch config
+    "$HOME/.config/nvim"      # Neovim config
+    "$HOME/.config/mpv/"      # mpv config
+    "$HOME/.vimrc"            # vim config
+    "$HOME/.config/hypr/"     # Hyprland config
+    "$HOME/.config/systemd/"  # systemd user services
+    "$HOME/.config/waybar/"   # Waybar config
+    "$HOME/.config/dunst/"    # Dunst config
+    "$HOME/.config/ranger/"   # Ranger config
+    "$HOME/.config/rofi/"     # Rofi config
+    "$HOME/.config/gtk*"      # GTK configs
 )
 
-# directories on ~/.config
-for source in "${list[@]}"; do
-    cp -r -u -v $source $dotfiles
-done
+case "$1" in 
 
-## data.txt
-cp -r -u -v ~/scripts/data.txt "$dotfiles"
+    --desktop)
+        # directories on ~/.config
+        for source in "${list[@]}"; do
+            cp -r -u -v $source $dotfiles
+        done
 
+        ## data.txt
+        cp -r -u -v ~/scripts/data.txt "$dotfiles"
 
+    ;;
+
+    --laptop)
+
+        cp -r -u -v  "$HOME/.config/hypr/" "$dotfiles/Laptop-configs/"
+        cp -r -u -v  "$HOME/.config/waybar/" "$dotfiles/Laptop-configs/"
+
+        ;;
+
+    --retrive)
+
+        echo "retriving configs"
+        rsync --info=progress2 -aAv --exclude={'hypr','waybar','Laptop-configs','i3','picom','polybar'} "$dotfiles" "$HOME/.config/"
+        cp -r -u -v "$dotfiles/Laptop-configs/*" "$HOME/.config/"
+
+        ;;
+    *) 
+    echo "Chose an option --desktop --laptop --retrive"
+
+esac
+    
 echo "All Done"
