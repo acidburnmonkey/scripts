146c1b655dc9af6890c8937deb66d8726d9adae6
acidburnmonkey <acidburnmonkey@gmail.com>
2025-10-04 00:11:47 -0400

    new file:   mouseConstrain.sh 	new file:   retired/virtuals.sh

---
+++ mouseConstrain.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+state="${HOME}/.local/state/togglemonitorlock"
+mkdir -p "$(dirname ${state})"
+booleanvalue="false"
+
+if [[ -f ${state} ]]; then
+    booleanvalue=$(cat ${state})
+fi
+
+if [[ ${booleanvalue} == "true" ]]; then
+    # Mouse can reach DP-5 (normal position)
+    hyprctl keyword monitor DP-5,3840x2160@60,-1920x-950,2
+    echo "false" > ${state}
+else
+    # Mouse blocked - move DP-5 very far away
+    hyprctl keyword monitor DP-5,3840x2160@60,50000x50000,2
+    echo "true" > ${state}
+fi
