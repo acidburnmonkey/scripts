6588da624dfa9787f0dad7d20000fd3fa02d0423
Acid <acidburnmonkey@gmail.com>
2026-03-12 18:43:00 -0400

    modified:   devmode.py

--- devmode.py
+++ devmode.py
@@ -10,15 +10,12 @@ home = pathlib.Path.home()
 
 # Python projects
 python_projects = {
+    "AlgoVictory": f'{home}/repos/divinatio',
     "mySite": f'{home}/repos/mysite/',
-    "Dashboard": f'{home}/python/Dashboard',
 }
 
 # other projects
 other = {"gobook": f'{home}/golang/book/ch1/'}
-
-# React projects
-react_projects = {}
 
 
 def main():
@@ -37,22 +34,18 @@ def main():
 
         choices["go"].append(key)
 
-    for key in react_projects.keys():
-        if "web" not in choices:
-            choices["web"] = []
-
         choices["web"].append(key)
 
     # Flatten all values into one list or is [[]]
     all_choices = [item for sublist in choices.values() for item in sublist]
 
-    selected = fzf.prompt(all_choices, '--prompt="Launch:"')
+    selection: list = fzf.prompt(all_choices, '--prompt="Launch:"')
 
-    if not selected:
+    if not selection:
         print('Nothing selected')
         exit(0)
 
-    selected = selected[0]
+    selected: str = selection[0]
     print("selected:", selected)
 
     # Handle selection
@@ -64,69 +57,37 @@ def main():
 
         os.chdir(project_path)
 
-        call_esac = esac()
         inner_cmd = "tmux new-window && exec zsh"
 
         tmux_cmd = ["tmux", "new", '-d', "-s", selected, "zsh", "-c", inner_cmd]
 
         subprocess.run(tmux_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        if call_esac:
-            other_commands = f"tmux send-keys -t {selected} 'sh ./esac.sh' C-m"
-            subprocess.run(
-                other_commands,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-                shell=True,
-            )
-
-        subprocess.run(
-            ['tmux', 'attach', '-t', selected],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-
-    # react
-    elif selected in react_projects.keys():
-
-        project_path = react_projects[selected]
-
-        subprocess.Popen(
-            [
-                "chromium-browser",
-                "--profile-directory='Default'",
-                "http://localhost:5173/",
-            ],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            stdin=subprocess.DEVNULL,
-            start_new_session=True,
-        )
-
-        subprocess.Popen(
-            ["firefox", "-p", "devfox"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            stdin=subprocess.DEVNULL,
-            start_new_session=True,
-        )
-
-        os.chdir(project_path)
-        subprocess.run(["tmux", "new", "-s", selected])
+        esac(selected)
 
     elif selected in other.keys():
-
         project_path = other[selected]
 
         os.chdir(project_path)
         subprocess.run(["tmux", "new", "-s", selected])
 
 
-def esac():
+def esac(selected: str):
     if 'esac.sh' in os.listdir():
-        return True
-    else:
-        return False
+        other_commands = f"tmux send-keys -t {selected} 'sh ./esac.sh' C-m"
+        subprocess.run(
+            other_commands,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            shell=True,
+        )
+
+    subprocess.run(
+        ['tmux', 'attach', '-t', selected],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
 
 if __name__ == "__main__":
     main()
